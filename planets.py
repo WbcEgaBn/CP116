d0ba16044241bf8fc90218d9fb36e9287aca0f62
brodriguez-cc <124939938+brodriguez-cc@users.noreply.github.com>
2025-02-06 13:53:56 -0700

    Reworked planets, 06, 1:53

--- planets.py
+++ planets.py
@@ -1,25 +1,29 @@
 #store data types as a list
 class Planet:
-    def __init__(self, position, mass, velocity, acc):
-        self.position = [1,1]
-        self.mass = 1
-        self.velocity = [1,1]
-        self.acc = acc
+    def __init__(self, position=None, mass=1, velocity=None, acc=0):
+        if velocity is None:
+            velocity = [0, 0]
+        if position is None:
+            position = [0, 0]
+        self.position = position[:]
+        self.mass = mass
+        self.velocity = velocity[:]
+        self.acc = acc[:]
 
     def get_position(self):
-        return self.position
+        return self.position[:]
     def set_position(self,position):
-        self.position = position
+        self.position = position[:]
     
     def get_acc(self):
-        return self.acc
+        return self.acc[:]
     def set_acc(self,acceleration):
-        self.acc = acceleration
+        self.acc = acceleration[:]
 
     def get_velocity(self):
-        return self.velocity
+        return self.velocity[:]
     def set_velocity(self, velocity):
-        self.velocity = velocity
+        self.velocity = velocity[:]
 
     def get_mass(self):
         return self.mass
