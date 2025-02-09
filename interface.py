d04e1808424110bfbd513a58b1a58ad3b033bffa
Gabe Itch <113611536+WbcEgaBn@users.noreply.github.com>
2025-02-09 16:50:29 -0700

    planets move and display with gravity

--- interface.py
+++ interface.py
@@ -2,14 +2,17 @@ import pygame as pg
 import pygame_widgets
 from pygame_widgets.button import Button
 from pygame_widgets.toggle import Toggle
-import Simulate
-from math import tan
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
+import simulate
+import planets
+from random import randint
 
 pg.init()
 screen = pg.display.set_mode((1200, 800))
 clock = pg.time.Clock()
 
-system = Simulate.Simulate()
+system = simulate.Simulate()
 
 planet_list = []
 planet_count = 0
@@ -19,17 +22,19 @@ draw_lines = []
 
 def create_planet(pos_x, pos_y):
     global planet_count
-    if planet_count < 3 : planet_count += 1
-    mass = 10
-    if planet_count < 4:
-        planet = pg.Rect(pos_x - (mass/2), pos_y - (mass/2), mass, mass)
-        planet_list.append(planet)
-        system.addBodies(planet)
+    planet_count += 1
+    mass = 1
+    color = (randint(0, 255), randint(0, 255), randint(0, 255))
+    math_planet = planets.Planets(position=[pos_x - (mass/2), pos_y - (mass/2)], mass=mass)
+    system.addBodies(math_planet)
+    planet = pg.Rect(pos_x - (mass/2), pos_y - (mass/2), mass*10, mass*10)
+    planet_list.append([planet, color])
 
 def clear_screen():
     global planet_list
     global planet_count
     planet_list = []
+    system.deleteBodies()
     planet_count = 0
     screen.fill((0, 0, 0))
 
@@ -49,23 +54,29 @@ def get_initial_velocity(pos_x0, pos_y0, pos_xf, pos_yf):
     dy = pos_yf - pos_y0
     v0_x = dx / t
     v0_y = dy / t
-    return v0_x, v0_y
+    return (v0_x, v0_y)
+
 
 def draw_text(text, font, color, x, y):
     img = font.render(text, True, color)
     screen.blit(img, (x, y))
 
-set_v0 = Toggle(screen, 925, 200, 50, 25)
+set_v0 = Toggle(screen, 925, 180, 50, 25)
+grav_slider = Slider(screen, 975, 300, 200, 25, min=1, max = 1000, step=10)
+grav_const_dis = TextBox(screen, 905, 280, 50, 50, fontSize=20)
+
+grav_const_dis.disable()  # Act as label instead of textbox
 
 frames = 0
 
 while True:
     screen.fill((0, 0, 0))
     clock.tick(24)
-    pg.draw.rect(screen, (255, 255, 255), [900, 0, 300, 300], 0)
+    pg.draw.rect(screen, (255, 255, 255), [900, 0, 300, 900], 0) # change 4th num
 
-    draw_text("Set v0 = 0", pg.font.SysFont("Arial", 20), (0, 0, 0), 1000, 200)
-
+    draw_text("Set v0 = 0", pg.font.SysFont("Arial", 20), (0, 0, 0), 1000, 180)
+    draw_text("Gravitational constant ↓ ", pg.font.SysFont("Arial", 20), (0, 0, 0), 950, 250)
+    grav_const_dis.setText(grav_slider.getValue())
     frames += 1
 
     if frames < 24*6:
@@ -79,30 +90,39 @@ while True:
     events = pg.event.get()
 
     for event in events:
+
         if event.type == pg.QUIT:
             pg.quit()
+
         if event.type == pg.MOUSEBUTTONDOWN: 
             pos_x0, pos_y0 = pg.mouse.get_pos()
             print(pos_x0, pos_y0)
+
             if pos_x0 < 900:
                 create_planet(pos_x0, pos_y0)
 
         if event.type == pg.MOUSEBUTTONUP:
             pos_xf, pos_yf = pg.mouse.get_pos()
             print(pos_xf, pos_yf)
+
             if set_v0.value == False:
                 get_initial_velocity(pos_x0, pos_y0, pos_xf, pos_yf)
 
     if running_simulation == True:
         system.run(0)
+        int = 0
+        planet_list_math = system.getBodies()
+        print(planet_list)
+        print(planet_list_math)
+
+        for planet in planet_list_math: 
+            planet_x = planet.get_position()[0]
+            planet_y = planet.get_position()[1]
+            planet_list[int] = [pg.Rect(planet_x, planet_y, 10, 10), planet_list[int][1]]
+            int += 1
 
     for i in range(len(planet_list)):
-        if i == 0:
-            curr_planet = pg.draw.rect(screen, "blue", planet_list[i])
-        elif i == 1:
-            curr_planet = pg.draw.rect(screen, "red", planet_list[i])
-        elif i == 2:
-            curr_planet = pg.draw.rect(screen, "yellow", planet_list[i])
+        curr_planet = pg.draw.rect(screen, planet_list[i][1], planet_list[i][0])
     
     pygame_widgets.update(events)
     pg.display.update()
