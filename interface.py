bb833368751e8f27987ea69d0e47fdf22308b765
Gabe Itch <113611536+WbcEgaBn@users.noreply.github.com>
2025-02-11 22:54:38 -0700

    +collision_const, +reset_time, reorder buttons

--- interface.py
+++ interface.py
@@ -10,7 +10,7 @@ from random import randint
 
 #start program objects
 pg.init()
-screen = pg.display.set_mode((1200, 820))
+screen = pg.display.set_mode((1200, 800))
 clock = pg.time.Clock()
 
 #base variables
@@ -20,7 +20,6 @@ planet_list = []
 planet_count = 0
 running_simulation = False
 
-draw_lines = []
 save_orbit_planet_list = []
 mass = 1
 
@@ -31,7 +30,7 @@ def create_planet(pos_x, pos_y):
     color = (randint(100, 255), randint(100, 255), randint(100, 255))
     math_planet = planets.Planets(position=[pos_x - (mass/2), pos_y - (mass/2)], mass=mass, color = color)
     system.addBodies(math_planet)
-    planet = pg.Rect(pos_x - (mass/2), pos_y - (mass/2), mass*10, mass*10)
+    planet = pg.Rect(pos_x - (10*mass/2), pos_y - (10*mass/2), mass*10, mass*10)
     planet_list.append([planet, color])
 
 #remove all planets from screen
@@ -39,6 +38,7 @@ def clear_screen():
     global planet_list
     global planet_count
     planet_list = []
+    system.set_time(0)
     system.deleteBodies()
     planet_count = 0
     screen.fill((0, 0, 0))
@@ -118,7 +118,6 @@ def two_body_orbit():
     y1 = 400
     x2 = 600
     y2 = 400
-
     system.deleteBodies()
     planet_list.clear()
     system.addBodies(planets.Planets(position=[x1 - (mass/2), y1 - (mass/2)], mass=mass, velocity=[0, 2], color=(255, 0, 0)))
@@ -132,24 +131,25 @@ def save_orbit():
     if len(planet_list) > 0:
         for planet in system.getBodies():
             save_orbit_planet_list.append(planet)
-    print(len(save_orbit_planet_list))
 
 #resets system to a saved var
 def load_saved_orbit():
-    print(len(save_orbit_planet_list))
     clear_screen()
     if len(save_orbit_planet_list) > 0:
         for planet in save_orbit_planet_list:
             create_planet(planet.get_position()[0], planet.get_position()[1])
 
 #create controls
-set_v0_to_0 = Toggle(screen, 925, 180, 50, 25)
+set_v0_to_0 = Toggle(screen, 925, 170, 50, 25)
 
-#tail
-show_trail = Toggle(screen, 925, 230, 50, 25)
+#trail
+show_trail = Toggle(screen, 925, 210, 50, 25)
+
+#show center of mass
+cm_value = Toggle(screen, 925, 250, 50,25)
 
 #gravity
-grav_const = TextBox(screen, 905, 300, 50, 50, fontSize=20)
+grav_const = TextBox(screen, 905, 310, 50, 50, fontSize=20)
 grav_const.disable()
 grav_slider = Slider(screen, 975, 320, 200, 25, min=1, max = 1000, step=10)
 
@@ -159,28 +159,30 @@ stop = Button(screen, 900, 50, 300, 50, text="Stop", inactiveColour=(255, 0, 0),
 reset = Button(screen, 900, 100, 300, 50, text="Reset", inactiveColour=(150, 150, 150), hoverColour=(150, 150, 150), onClick=lambda: clear_screen())
 
 #velocity
-t_const = Slider(screen, 975, 420, 200, 25, min=0.01, max = 0.1, step=0.01, initial=0.05)
-t_const_display = TextBox(screen, 905, 400, 50, 50, fontSize=20)
+t_const = Slider(screen, 975, 390, 200, 25, min=0.01, max = 0.1, step=0.01, initial=0.05)
+t_const_display = TextBox(screen, 905, 380, 50, 50, fontSize=20)
 
 #time step
-t_step = Slider(screen, 975, 520, 200, 25, min=.1, max = 5.0, step = 0.25, initial = 1)
-t_step_display = TextBox(screen, 905, 500, 50, 50, fontSize=20)
+t_step = Slider(screen, 975, 460, 200, 25, min=.1, max = 5.0, step = 0.25, initial = 1)
+t_step_display = TextBox(screen, 905, 450, 50, 50, fontSize=20)
 
 #mass
-mass_slider = Slider(screen, 975, 620, 200, 25, min=.5, max = 5.0, step = .5, initial = 1)
+mass_slider = Slider(screen, 975, 530, 200, 25, min=.5, max = 5.0, step = .5, initial = 1)
+mass_slider_display = TextBox(screen, 905, 520, 50, 50, fontSize=20)
+
+#collision constant
+collision_slider = Slider(screen, 975, 600, 200, 25, min=1, max=20, step=1, initial = 10)
+collision_display = TextBox(screen, 905, 590, 50, 50, fontSize=20)
 
 #preloaded systems
-design_1 = Button(screen, 900, 670, 100, 50, text="Triangle", inactiveColour=(255, 255, 0), hoverColour=(255, 255, 0), fontSize=18, onClick=lambda: scalene())
-design_2 = Button(screen, 1000, 670, 100, 50, text="Trapezoid", inactiveColour=(255, 0, 255), hoverColour=(255, 0, 255), fontSize=20, onClick=lambda: trapezoid())
-design_3 = Button(screen, 1100, 670, 100, 50, text="Orbit", inactiveColour=(0, 255, 255), hoverColout=(0, 255, 255), fontSize=18, onClick=lambda: two_body_orbit())
+design_1 = Button(screen, 900, 650, 100, 50, text="Triangle", inactiveColour=(255, 255, 0), hoverColour=(255, 255, 0), fontSize=18, onClick=lambda: scalene())
+design_2 = Button(screen, 1000, 650, 100, 50, text="Trapezoid", inactiveColour=(255, 0, 255), hoverColour=(255, 0, 255), fontSize=20, onClick=lambda: trapezoid())
+design_3 = Button(screen, 1100, 650, 100, 50, text="Orbit", inactiveColour=(0, 255, 255), hoverColout=(0, 255, 255), fontSize=18, onClick=lambda: two_body_orbit())
 
 #save and load designs
-save_design = Button(screen, 900, 720, 300, 50, text="Save Orbit", inactiveColour=(200, 200, 255), hoverColour=(200, 200, 255), fontSize=18, onClick=lambda: save_orbit())
-load_saved_design = Button(screen, 900, 770, 300, 50, text="Load Saved Orbit", inactiveColour=(200, 255, 200), hoverColour=(200, 255, 200), fontSize=18, onClick=lambda: load_saved_orbit())
+save_design = Button(screen, 900, 700, 300, 50, text="Save Orbit", inactiveColour=(200, 200, 255), hoverColour=(200, 200, 255), fontSize=18, onClick=lambda: save_orbit())
+load_saved_design = Button(screen, 900, 750, 300, 50, text="Load Saved Orbit", inactiveColour=(200, 255, 200), hoverColour=(200, 255, 200), fontSize=18, onClick=lambda: load_saved_orbit())
 frames = 0
-
-#show center of mass
-cm_value = Toggle(screen, 250,25, 50,25)
 
 last_event_mouse_down = False
 
@@ -193,20 +195,23 @@ while True:
     clock.tick(60 / system.get_time_step())
     pg.draw.rect(screen, (255, 255, 255), [900, 0, 300, 900], 0) # change 4th num
 
-    draw_text("Set v0 = 0", pg.font.SysFont("Arial", 20), (0, 0, 0), 1000, 180)
-    draw_text("Show Planet Trail", pg.font.SysFont("Arial", 20), (0, 0, 0), 1000, 230)
-    draw_text("Gravitational Constant ↓ ", pg.font.SysFont("Arial", 20), (0, 0, 0), 970, 280)
+    draw_text("Set v0 = 0", pg.font.SysFont("Arial", 20), (0, 0, 0), 1000, 170)
+    draw_text("Show Planet Trail", pg.font.SysFont("Arial", 20), (0, 0, 0), 1000, 210)
+    draw_text("Show System's CM", pg.font.SysFont("Arial", 20), (0, 0, 0), 1000, 250)
+    draw_text("Gravitational Constant ↓ ", pg.font.SysFont("Arial", 20), (0, 0, 0), 970, 290)
+    draw_text("Collision Constant ↓ ", pg.font.SysFont("Arial", 20), (0, 0, 0), 1000, 570)
     grav_const.setText(grav_slider.getValue())
     system.set_g_constant(grav_slider.getValue())
+    collision_display.setText(collision_slider.getValue())
+    system.set_collide_const(collision_slider.getValue())
 
     #stuff for time_step
-    draw_text("Time Step ↓ ", pg.font.SysFont("Arial", 20), (0, 0, 0), 1020, 485)
+    draw_text("Time Step ↓ ", pg.font.SysFont("Arial", 20), (0, 0, 0), 1025, 430)
     t_step_display.setText(round(t_step.getValue(), 2))
     system.set_time_step(round(t_step.getValue(), 2))
 
     #add mass for planets
-    draw_text("Mass ↓", pg.font.SysFont("Arial", 20), (0, 0, 0), 1000, 585)
-    mass_slider_display = TextBox(screen, 905, 600, 50, 50, fontSize=20)
+    draw_text("Mass ↓", pg.font.SysFont("Arial", 20), (0, 0, 0), 1050, 500)
     mass_slider_display.setText(round(mass_slider.getValue(), 2))
     mass = mass_slider.getValue()
 
@@ -218,7 +223,7 @@ while True:
     frames += 1
 
     if set_v0_to_0.value == False:
-        draw_text("Yeet Constant ↓ ", pg.font.SysFont("Arial", 20), (0, 0, 0), 1000, 380)
+        draw_text("Yeet Constant ↓ ", pg.font.SysFont("Arial", 20), (0, 0, 0), 1000, 360)
         t_const_display.setText(t_const.getValue())
     if frames < 150:
         draw_text("Click and drag to add planets and set v0.", pg.font.SysFont("Arial", 20), (255, 255, 255), 300, 50)
@@ -266,8 +271,6 @@ while True:
             planet_list.append([system.lisBodies[-1], color])
             system.lisBodies[-1].set_color(color)
 
-
-
         #keep track of index
         integer = 0
         planet_list_math = system.getBodies()
@@ -280,12 +283,6 @@ while True:
             planet_list[integer] = temp
             integer += 1
 
-        #
-        # print('///sys')
-        # print(system.getBodies())
-        # print('///planet')
-        # print(planet_list)
-
     #redraw system
     for i in range(len(planet_list)):
         pg.draw.rect(screen, planet_list[i][1], planet_list[i][0], border_radius= int((system.getBodies()[i].get_mass() * 3) ** 2))
@@ -294,4 +291,3 @@ while True:
 
     pygame_widgets.update(events)
     pg.display.update()
-
